186da5f8136ff91377c9dd2358180c2f18d4810d
meagdoh <meagdoherty@gmail.com>
2017-04-25 17:15:56 -0400

    Simplified seed data upload

--- got/db/seeds.rb
+++ got/db/seeds.rb
@@ -12,8 +12,6 @@ lannister = House.create(name:"Lannister", img_url:"https://s-media-cache-ak0.pi
 tyrell = House.create(name:"Tyrell", img_url:"https://s-media-cache-ak0.pinimg.com/originals/94/01/3f/94013f53e379a044bf64654b3431c495.jpg")
 tully = House.create(name:"Tully", img_url:"https://mir-s3-cdn-cf.behance.net/project_modules/disp/6f89047933845.560b452f50b32.jpg")
 
-characters = Character.create([
-  {name: "Margaery Tyrell", img_url: "http://screenprism.com/assets/img/article/o57_1fsvLoZl.jpg", house_id: tyrell.id},
-  {name: "Jaime Lannister", img_url: "http://screenprism.com/assets/img/article/jaime-lannister.jpg", house_id: lannister.id},
-  {name: "Brynden Tully", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/6/61/6x07_Blackfish.png/revision/latest/scale-to-width-down/350?cb=20160601191023", house_id: tully.id}
-  ])
+tyrell.characters.create(name:"Margaery Tyrell", img_url: "http://screenprism.com/assets/img/article/o57_1fsvLoZl.jpg")
+lannister.characters.create(name:"Jaime Lannister", img_url: "http://screenprism.com/assets/img/article/jaime-lannister.jpg")
+tully.characters.create(name:"Brynden Tully", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/6/61/6x07_Blackfish.png/revision/latest/scale-to-width-down/350?cb=20160601191023")
