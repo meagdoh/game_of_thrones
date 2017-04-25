3c4496c6727d9762fa82741abfd1474039fd0111
meagdoh <meagdoherty@gmail.com>
2017-04-25 17:29:35 -0400

    Change initial routes to plural case

--- got/config/routes.rb
+++ got/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :house
-  resource :character
+  resource :houses
+  resource :characters
 end
