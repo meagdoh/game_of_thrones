e7bfd0b1635a932b0202911ea8d13fa07f394aed
meagdoh <meagdoherty@gmail.com>
2017-04-26 12:18:48 -0400

    Update seed.rb. Connect characters to house

--- got/db/seeds.rb
+++ got/db/seeds.rb
@@ -8,10 +8,10 @@
 Character.destroy_all
 House.destroy_all
 
-lannister = House.create(name:"Lannister", img_url:"https://s-media-cache-ak0.pinimg.com/736x/d0/b2/c1/d0b2c1070a34ff44acf356b86bb6be64.jpg")
-tyrell = House.create(name:"Tyrell", img_url:"https://s-media-cache-ak0.pinimg.com/originals/94/01/3f/94013f53e379a044bf64654b3431c495.jpg")
-tully = House.create(name:"Tully", img_url:"https://mir-s3-cdn-cf.behance.net/project_modules/disp/6f89047933845.560b452f50b32.jpg")
+lannister = House.create({name:"Lannister", img_url:"https://s-media-cache-ak0.pinimg.com/736x/d0/b2/c1/d0b2c1070a34ff44acf356b86bb6be64.jpg"})
+tyrell = House.create({name:"Tyrell", img_url:"https://s-media-cache-ak0.pinimg.com/originals/94/01/3f/94013f53e379a044bf64654b3431c495.jpg"})
+tully = House.create({name:"Tully", img_url:"https://mir-s3-cdn-cf.behance.net/project_modules/disp/6f89047933845.560b452f50b32.jpg"})
 
-tyrell.characters.create(name:"Margaery Tyrell", img_url: "http://screenprism.com/assets/img/article/o57_1fsvLoZl.jpg")
-lannister.characters.create(name:"Jaime Lannister", img_url: "http://screenprism.com/assets/img/article/jaime-lannister.jpg")
-tully.characters.create(name:"Brynden Tully", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/6/61/6x07_Blackfish.png/revision/latest/scale-to-width-down/350?cb=20160601191023")
+tyrell.characters.create({name:"Margaery Tyrell", img_url: "http://screenprism.com/assets/img/article/o57_1fsvLoZl.jpg"})
+lannister.characters.create({name:"Jaime Lannister", img_url: "http://screenprism.com/assets/img/article/jaime-lannister.jpg"})
+tully.characters.create({name:"Brynden Tully", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/6/61/6x07_Blackfish.png/revision/latest/scale-to-width-down/350?cb=20160601191023"})
